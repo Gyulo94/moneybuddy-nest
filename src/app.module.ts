98aa6158c348c65a2f914db95032c2b8f95fc235
Gyulo94 <qkrcjfrb112@gmail.com>
2025-03-23 06:12:31 -0300

    미들웨어 로거 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthModule } from './auth/auth.module';
 import { JwtGuard } from './auth/guards/jwt.guard';
 import { RoleGuard } from './auth/guards/role.guard';
 import { CommonModule } from './common/common.module';
+import { RequestMiddleware } from './common/utils/middleware';
 import { EmailModule } from './email/email.module';
 import { UserModule } from './user/user.module';
 
@@ -28,4 +34,10 @@ import { UserModule } from './user/user.module';
     },
   ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(RequestMiddleware)
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
+  }
+}
